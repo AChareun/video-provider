c5d04ee1490fd211a5394601815528823508441c
AChareun <achareun22@gmail.com>
2021-01-11 21:32:56 -0300

    implement title service with unit test

---
+++ src/module/title/service/__test__/titleService.test.js
@@ -0,0 +1,22 @@
+const TitleService = require('../titleService');
+
+const repositoryMock = {
+    getPaginated: jest.fn(),
+    getById: jest.fn(),
+};
+
+const testService = new TitleService(repositoryMock);
+
+test('TitleService method getPaginated should call correct TitleRepository method ', () => {
+    testService.getPaginated(1, 0);
+
+    expect(repositoryMock.getPaginated).toHaveBeenCalledTimes(1);
+    expect(repositoryMock.getPaginated).toHaveBeenCalledWith(1, 0);
+});
+
+test('TitleService method getById should call correct TitleRepository method ', () => {
+    testService.getById(1);
+
+    expect(repositoryMock.getById).toHaveBeenCalledTimes(1);
+    expect(repositoryMock.getById).toHaveBeenCalledWith(1);
+});
