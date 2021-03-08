34c42d8cfe3b8f78a61855cc4f4abff0fffc5cc1
AChareun <achareun22@gmail.com>
2021-03-08 15:33:35 -0300

    implement httpreqadapter using axios

---
+++ src/lib/httpReqAdapter.ts
@@ -0,0 +1,10 @@
+import Axios from 'axios';
+
+function getAxiosInstance() {
+    return Axios.create();
+}
+
+export function get(url: string){
+    const axios = getAxiosInstance();
+    return axios.get(url);
+}
